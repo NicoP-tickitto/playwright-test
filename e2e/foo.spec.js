f2e706b87fb668d4bb5d1eb251a334e5c42f1dac
Niccolo <niccolo@tickitto.com>
2021-06-29 15:10:45 +0200

    add test

--- e2e/foo.spec.js
+++ e2e/foo.spec.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('basic test', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('http://localhost:3000');
   expect(await page.screenshot()).toMatchSnapshot('snapshot-name.png');
   // const name = await page.innerText('.navbar__title');
   // expect(name).toBe('Playwright');
